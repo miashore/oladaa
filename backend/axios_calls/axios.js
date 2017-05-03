1c2c425b10c1a7a13702463aad81ae71998192d3
Sean Mee <Sean Mee>
2017-05-03 07:10:12 -0700

    added axios call for user registration

--- backend/axios_calls/axios.js
+++ backend/axios_calls/axios.js
@@ -1,17 +1,23 @@
-var username = 'Sean';
-var password = 'Munchen*86';
+// var username = 'Sean';
+// var password = 'Munchen*86';
 
-
+//**********************************************************************************************************************
 
 //axios call for getting mock data from our user personas.  Will return an object that contains the three users and each
 //user will have three different properties; fatBurn, cardio, and peak.
-axios.get('../mock_data/mockData.json').then(resp=>{
-    console.log(resp.data);
-});
 
-//axios call for verifying login information.  Send the username and password to our server and check for their
+
+/*axios.get('../mock_data/mockData.json').then(resp=>{
+    console.log(resp.data);
+});*/
+
+//**********************************************************************************************************************
+
+//axios call for verifying login information.  Send the username and password to login.php and check for their
 //existence in our database, it returns a 1 for true and a 0 for false.
-axios.post('../user_verification_php/login.php', {username, password}).then(resp=>{
+
+
+/*axios.post('../user_verification_php/login.php', {username, password}).then(resp=>{
     console.log('Our response from the server ', resp.data);
     if(resp.data === 0){
         document.writeln('Invalid Username');
@@ -21,8 +27,20 @@ axios.post('../user_verification_php/login.php', {username, password}).then(resp
     }
 }).catch(err=>{
     console.log(err);
-});
+});*/
 
+//**********************************************************************************************************************
 
+//axios call for registering a new user.  Send the new users username, password, and email to register.php.  If the
+// username or email already exists, or not all fields are filled it will return an error message of user registration
+// failed.
+
+/*var username = 'Bob';
+var password = 'HiImBob';
+var email = "bob@beativities.com";
+
+axios.post('../user_verification_php/register.php', {username, password, email}).then(resp=>{
+    console.log('Our response from register.php ', resp);
+});*/
 
 
