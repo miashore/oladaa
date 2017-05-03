cf5856c06f86d9e656ec6432f25f5de7cbc848fd
Kevin Nguyen <kngwin9@gmail.com>
2017-05-02 19:39:28 -0700

    Saving partial complete on the register form, will need to include css and routing.

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -2,30 +2,25 @@ import React from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import './app.css';
 import Header from './header/header';
-<<<<<<< HEAD
-import RegisterForm from './register_new_user/register_new_user'
-=======
 import Footer from './footer/footer';
->>>>>>> af3df51bb0e1b9e4ca7ab29344272e7f8b617081
 
-const App = (props) => {
-    injectTapEventPlugin();
 
-    return (
-        <div>
+class App extends Component {
+
+    componentWillMount() {
+        injectTapEventPlugin();
+    }
+
+    render() {
+
+        return (
             <div>
                 <Header />
+                { props.children }
+                <Footer />
             </div>
-            { props.children }
-<<<<<<< HEAD
-            <div>
-                <RegisterForm />
-            </div>
-=======
-            <Footer />
->>>>>>> af3df51bb0e1b9e4ca7ab29344272e7f8b617081
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default App;
--- client/src/components/register_new_user/register_new_user.js
+++ client/src/components/register_new_user/register_new_user.js
@@ -1,21 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 class RegisterForm extends Component {
-
     submitForm(idk) {
         console.log('Form submitted: ', idk);
     }
-
+    componentWillMount() {
+        injectTapEventPlugin();
+    }
+    renderTextField = ({ input, label, meta: { touched, error }, ...custom }) => (
+        <TextField hintText={label}
+                   floatingLabelText={label}
+                   errorText={touched && error}
+                   {...input}
+                   {...custom}
+        />
+    );
     render() {
+        const button_style = { marginLeft: '2%', marginRight: '2%', marginTop: '1%'};
         const { handleSubmit } = this.props;
         return (
             <div>
-                Works!
+                <form onSubmit={ handleSubmit( (formValue) => {this.submitForm(formValue)})}>
+                    <div className="form-group">
+                        <Field name="username"
+                               component={this.renderTextField}
+                               type="text"
+                               label="Username"
+                               />
+                    </div>
+                    <div className="form-group">
+                        <Field name="email"
+                               component={this.renderTextField}
+                               type="text"
+                               label="Email"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <Field name="password"
+                               component={this.renderTextField}
+                               type="password"
+                               label="Password"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <Field name="password_confirmation"
+                               component={this.renderTextField}
+                               type="password"
+                               label="Password Confirmation"
+                        />
+                    </div>
+                    <RaisedButton style={button_style} type="submit" label="Register" primary={true} />
+                    <RaisedButton label="Sign In" secondary={true} />
+                </form>
             </div>
         )
     }
-
 }
+RegisterForm = reduxForm({
+    form: 'addForm'
+})(RegisterForm);
 
 export default RegisterForm;
