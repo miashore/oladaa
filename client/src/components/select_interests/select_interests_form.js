fab51eb2857571651bdc50f2a7ce4aa29a6cf294
Kevin Nguyen <kngwin9@gmail.com>
2017-05-03 10:10:23 -0700

    Saving

--- client/src/components/select_interests/select_interests_form.js
+++ client/src/components/select_interests/select_interests_form.js
@@ -5,6 +5,7 @@ import Checkbox from 'material-ui/Checkbox'
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 class SelectInterests extends Component {
+
     submitForm(idk) {
         console.log('Form submitted: ', idk);
     }
@@ -13,18 +14,19 @@ class SelectInterests extends Component {
     }
     renderCheckbox = ({ input, label }) => (
     <Checkbox label={label}
+              value=
               checked={input.value ? true : false}
               onCheck={input.onChange}/>
-)
+    );
     render() {
         const button_style = { marginLeft: '2%', marginRight: '2%', marginTop: '1%'};
-        const { handleSubmit } = this.props;
+        const { handleSubmit, val } = this.props;
         return (
             <div>
                 <h1>Please Select At Least 3 Interests:</h1>
                 <form onSubmit={ handleSubmit( (formValue) => {this.submitForm(formValue)})}>
                     <div>
-                        <Field name="sports_fitness" value="1" component={this.renderCheckbox} label="Sports & Fitness"/>
+                        <Field name="sports_fitness" component={this.renderCheckbox} label="Sports & Fitness"/>
                         <Field name="dance" component={this.renderCheckbox} label="Dance"/>
                         <Field name="outdoors_adventures" component={this.renderCheckbox} label="Outdoors & Adventures"/>
                         <Field name="health_wellness" component={this.renderCheckbox} label="Health & Wellness"/>
@@ -38,7 +40,6 @@ class SelectInterests extends Component {
                         <Field name="film" component={this.renderCheckbox} label="Film"/>
                     </div>
                     <RaisedButton style={button_style} type="submit" label="Submit" primary={true} />
-
                 </form>
             </div>
         )
