750425f2f21984d7b39bfdb9becb92822082db07
miashore <mcbashore@gmail.com>
2017-05-04 12:18:28 -0700

    created an accordion for all events available to the user

--- client/src/components/view_all_events/view_all.js
+++ client/src/components/view_all_events/view_all.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { Card, CardHeader } from 'material-ui/Card';
+import { Card, CardHeader, CardText } from 'material-ui/Card';
+import Paper from 'material-ui/Paper';
 import EventCard from '../event_card/event_card';
 
 const ViewAllEventsList = () => {
     return (
-        <Card>
-            <CardHeader actAsExpander={true} showExpandableButton={true} title="Event Name" subtitle="Event Subtitle" />
-            <EventCard />
-        </Card>
+        <Paper zDepth={3}>
+            <Card>
+                <CardHeader actAsExpander={true} showExpandableButton={true} title="Sports & Fitness" />
+                <CardText expandable={true}>
+                    <EventCard />
+                </CardText>
+            </Card>
+            <Card>
+                <CardHeader actAsExpander={true} showExpandableButton={true} title="Pets" />
+                <CardText expandable={true}>
+                    <EventCard />
+                </CardText>
+            </Card>
+            <Card>
+                <CardHeader actAsExpander={true} showExpandableButton={true} title="Film" />
+                <CardText expandable={true}>
+                    <EventCard />
+                    <EventCard />
+                    <EventCard />
+                    <EventCard />
+                </CardText>
+            </Card>
+        </Paper>
     )
 };
 
