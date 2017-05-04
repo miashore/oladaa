8d68550df966182ae449c02c8afd975793331fb3
miashore <mcbashore@gmail.com>
2017-05-04 09:31:38 -0700

    imported view all list into app to see it with header and footer

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './header/header';
 import Footer from './footer/footer';
+import ViewAllList from './view_all_events/view_all';
 
 class App extends Component {
 
@@ -10,6 +11,7 @@ class App extends Component {
             <div>
                 <Header />
                 { this.children }
+                <ViewAllList />
                 <Footer />
             </div>
         );
