a70e8b83ec7bca09b0503295d276e49c8d674f1f
Kevin Nguyen <kngwin9@gmail.com>
2017-05-04 18:08:33 -0700

    saving to pull redux-thunk

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,24 @@
+// import axios from 'axios';
 // import { REGISTER_USER } from './types';
+//
+// const username = 'Dan';
+// const password = 'HiImDan';
+// const email = "dan@beativities.com";
+//
+// axios.post('../server/register.php', {username, password, email}).then(resp=>{
+//     console.log('Our response from register.php ', resp);
+//     axios.post('../server/login.php', {username, password}).then(resp=>{
+//         console.log('Our response from the server ', resp.data);
+//         if(resp.data === 0){
+//             console.log('Invalid Username');
+//         }
+//         else{
+//             console.log('User logged in');
+//         }
+//     }).catch(err=>{
+//         console.log(err);
+//     });
+// });
 //
 // export function register_user() {
 //     return {
