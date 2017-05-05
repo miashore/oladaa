5022dd4807e0ae68683fff36bbf95749599a53de
Kevin Nguyen <kngwin9@gmail.com>
2017-05-04 22:09:57 -0700

    saving location modal

---
+++ client/src/components/location/location_modal.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Location from './location';
+
+class LocationModal extends Component {
+    render(){
+        return(
+            <div>
+            <Location/>
+            </div>
+        )
+    }
+}
+
+export default LocationModal;
