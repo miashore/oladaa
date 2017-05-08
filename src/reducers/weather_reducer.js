0d1706d2a29787c93460a5d14a32ac74887baced
miashore <mcbashore@gmail.com>
2017-05-08 14:36:55 -0700

    saving to pull from dev

--- src/reducers/weather_reducer.js
+++ src/reducers/weather_reducer.js
@@ -1,14 +1,17 @@
 import { FETCH_WEATHER } from '../actions/types';
 
 const default_state = {
-
+    weather: null
 };
 
 export default function(state = default_state, action){
     switch(action.type){
         case FETCH_WEATHER:
             console.log('Weather Reducer: ', action.payload, action.payload);
-            return { ...state,};
+            return {
+                ...state,
+                weather: action.payload
+            };
         default:
             return state;
     }
