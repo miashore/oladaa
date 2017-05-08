5a14b435fbd5e339d7aafac9692a67a99e704603
miashore <mcbashore@gmail.com>
2017-05-08 16:31:00 -0700

    experimenting with a switch statement for the icon codes

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -6,11 +6,55 @@ class Home extends Component {
     componentWillMount(){
         this.props.fetchWeather(this.props.location);
     }
+    componentWillReceiveProps(nextProps){
+        this.chooseBackground(nextProps.weather.iconID);
+    }
+
+    chooseBackground(code){
+        let bkgdImage = '../imgs/weather/';
+        switch(code) {
+            case '11d':
+            case '11n':
+                bkgdImage += 'thunderstorm.jpg';
+                break;
+            case '09d':
+            case '09n':
+                bkgdImage += 'drizzle.jpg';
+                break;
+            case '10d':
+            case '10n':
+                bkgdImage += 'rain.jpg';
+                break;
+            case '13d':
+            case '13n':
+                bkgdImage += 'snow.jpg';
+                break;
+            case '50d':
+            case '50n':
+                bkgdImage += 'mist.jpg';
+                break;
+            case '01d':
+            case '01n':
+                bkgdImage += 'clear.jpg';
+                break;
+            case '02d':
+            case '02n':
+            case '03d':
+            case '03n':
+            case '04d':
+            case '04n':
+                bkgdImage += 'cloudy.jpg';
+                break;
+            default:
+                bkgdImage += 'default.jpeg';
+        }
+        console.log(bkgdImage);
+    }
 
     render() {
 
         const styles = {
-            mid_container: {height: '100%', width: '100%', background: 'lightblue' }
+            mid_container: {height: '100%', width: '100%', background: 'lightblue'}
         };
 
         return (
@@ -26,7 +70,8 @@ class Home extends Component {
 function mapStateToProps(state){
     console.log('Home state: ', state);
     return {
-        location: state.location.coords
+        location: state.location.coords,
+        weather: state.weather.weather
     }
 }
 
