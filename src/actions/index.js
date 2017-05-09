aab743e91a754514440145eda11fe2470bfffbac
Sean Mee <Sean Mee>
2017-05-09 09:36:42 -0700

    code cleanup

--- src/actions/index.js
+++ src/actions/index.js
@@ -82,9 +82,7 @@ export function logout_user(){
     }
 }
 
-//const MEETUP_URL = 'https://api.meetup.com/2/open_events?and_text=False&offset=0&format=json&lon=-117.79&limited_events=False&text_format=plain&photo-host=public&page=50&radius=10&lat=33.68&desc=False&status=upcoming&category=32';
 const MU_KEY = '&key=1012337b1a2c2a5974255a4412b237a';
-const category_id = 32;
 
 export function fetchEvents(coords){
     console.log('Coords: ', coords);
