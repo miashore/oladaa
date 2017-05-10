e0260e5de89d2083bde40e9cdd074de1fd02376a
miashore <mcbashore@gmail.com>
2017-05-10 12:01:23 -0700

    added darksky api and fetchweather works on the home screen

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,28 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchWeather } from '../../actions/index';
-import CircularProgress from 'material-ui/CircularProgress';
 
 class Home extends Component {
     componentWillMount(){
         this.props.fetchWeather(this.props.location);
     }
 
-    renderImage(){
-        if(this.props.weather.iconID === undefined) {
-            return (
-                <CircularProgress />
-            )
-        }
-        return (
-            <img src={this.props.weather.iconImg}/>
-        )
-    }
     render() {
         const { weather } = this.props;
 
         const styles = {
-            mid_container: {height: '100vh', width: '100%', background: 'url(./src/components/imgs/weather/'+this.props.weather.background+') no-repeat center center fixed'},
+            mid_container: {height: '100vh', width: '100%', 'backgroundImage': `url(../src/components/imgs/weather/${weather.background})`, backgroundPosition: 'center center', backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed', backgroundSize: 'cover'},
             weather_div: {zIndex: '5', position: 'fixed', top: '10vh', left: '2vh'},
             weather_icon: {margin: '0'},
             location_text: {margin: '3% 2% 0 1%', display: 'inline-block', verticalAlign: 'top', fontSize: '1.2em'},
@@ -32,9 +21,8 @@ class Home extends Component {
             <div>
                 <div style={styles.mid_container}/>
                 <div style={styles.weather_div} >
-                    { this.renderImage() }
-                    <p style={styles.location_text}>{weather.location}</p>
-                    <p>{weather.main_description}</p>
+                    <p style={styles.location_text}>{weather.timezone}</p>
+                    <p>{weather.summary}</p>
                 </div>
             </div>
         )
@@ -42,7 +30,6 @@ class Home extends Component {
 }
 
 function mapStateToProps(state){
-    console.log('Home state: ', state);
     return {
         location: state.location.coords,
         weather: state.weather.weather
