9363e1e77d073ec87650e7eb0fb91105a244c9ec
miashore <mcbashore@gmail.com>
2017-05-11 17:14:07 -0700

    added more recommended events styling

--- src/components/recommended_events/recommended_events.js
+++ src/components/recommended_events/recommended_events.js
@@ -10,20 +10,24 @@ class RecommendedEventsList extends Component {
     render () {
         const styles = {
             body: { width: '90vw', background: 'rgba(255, 255, 255, 0.2)', margin: '3vw auto 0' },
+            innerBody: { width: '90vw', background: 'rgba(255, 255, 255, 0.2)', margin: '3vw auto 0', paddingBottom: '2%' },
+            populatedBody: {width: '90vw', background: 'rgba(255, 255, 255, 0.8)', margin: '3vw auto 0'},
             error: { width: '75vw', background: 'rgba(255, 255, 255, 0.8)', margin: '0 auto 4vw', padding: '3%' },
             center: { textAlign: 'center' },
-            button: {zIndex: 0, width: '75%', margin: '7% 0 0 0'}
+            button: {zIndex: 0, width: '60vw', margin: '5% auto'}
         };
 
         if(this.props.auth === true && this.props.fitbit === undefined){
             return (
                 <Paper zDepth={3} style={styles.body}>
-                    <Card style={styles.body}>
+                    <Card style={styles.innerBody}>
                         <CardTitle title="Your Recommended Events" />
                         <Paper zDepth={2} style={styles.error}>
-                            <h2 style={styles.center}>Please Connect Your Fitbit to See Recommended Events</h2>
-                            <RaisedButton style={styles.button} containerElement={<Link to="/fitbit_login"/>} label="Connect to FitBit"
-                                          secondary={true}/>
+                            <div style={styles.innerBody}>
+                                <h3 style={styles.center}>Please Connect Your Fitbit to See Your Recommended Events</h3>
+                                <RaisedButton style={styles.button} containerElement={<Link to="/fitbit_login"/>} label="Connect to FitBit"
+                                              secondary={true}/>
+                            </div>
                         </Paper>
                     </Card>
                 </Paper>
@@ -31,8 +35,8 @@ class RecommendedEventsList extends Component {
         }
 
         return (
-            <Paper zDepth={3} style={styles.body}>
-                <Card style={styles.body}>
+            <Paper zDepth={3} style={styles.populatedBody}>
+                <Card style={styles.innerBody}>
                     <CardTitle title="Your Recommended Events" />
                     <EventCard />
                 </Card>
